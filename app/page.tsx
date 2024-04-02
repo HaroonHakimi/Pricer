c2eb8efa473c3dd1f8a6827ff55cf682be03e772
HaroonHakimi <haroonhakimi2005@gmail.com>
2024-04-02 18:00:05 -0400

    comleted mongoose

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import Image from "next/image";
 import Searchbar from "@/components/Searchbar";
 import HeroCarousel from "@/components/HeroCarousel";
+import { getAllProducts } from "@/lib/actions";
+import ProductCard from "@/components/ProductCard";
 
-const Home = () => {
+const Home =  async () => {
+  const allProducts = await getAllProducts()
+
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -41,8 +45,8 @@ const Home = () => {
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {
-            ['Apple Iphone 15', 'Sneakers', 'Book'].map((product) => (
-              <div>{product}</div>
+            allProducts?.map((product) => (
+              <ProductCard key={product._id} product={product}/>
             ))
           }
         </div>

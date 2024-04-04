f48b503b577b6f47d6ad2cb437854e49c4a044ad
HaroonHakimi <haroonhakimi2005@gmail.com>
2024-04-04 01:09:52 -0400

    Worked on sending users emails

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { FormEvent, Fragment, useState } from "react";
 import Image from "next/image";
 
 const Modal = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault
+    setIsSubmitting(true)
+
+    //await addUserEmailTOProduct(productId, email)
+
+    setIsSubmitting(false)
+    setEmail('')
+    closeModal()
+  }
 
   const openModal = () => {
     setIsOpen(true);
@@ -35,7 +48,10 @@ const Modal = () => {
                 <Dialog.Overlay className='fixed inset-0'/>
             </Transition.Child>
 
-        <span></span>
+        <span 
+        className="inline-block h-screen align-middle"
+        aria-hidden='true'
+        />
             <Transition.Child
             as={Fragment}
             enter='ease-out duration-300'
@@ -47,16 +63,68 @@ const Modal = () => {
             >
                 <div className="dialog-content">
                     <div  className="flex flex-col">
-                        <div className="flex justify-center">
+                        <div className="flex justify-between">
                             <div className="p-3 border border-gray-200 rounded-10">
                                 <Image
-                                src='/assets/icons.logo.svg'
+                                src='/assets/icons/logo.svg'
                                 alt='logo'
                                 width={28}
                                 height={28}
                                 />
                             </div>
+                            <Image
+                            src='/assets/icons/x-close.svg'
+                            alt="close"
+                            width={24}
+                            height={24}
+                            className="cursor-pointer"
+                            onClick={closeModal}
+                            />
                         </div>
+                        <h4 className="dialog-head_text">
+                          Stay updated with product pricing alers right in you inbox!
+                        </h4>
+
+                        <p className="text-sm text-gray-600 mt-2">
+                          Never miss a bargain again with out timely alerts!
+                        </p>
+
+                        <form 
+                        className="flex flex-col mt-5"
+                        onSubmit={handleSubmit}
+                        >
+                          <label htmlFor="email" className="text-sm font-medium text-gray-700">
+                            Email Address
+                          </label>
+                          <div className="dialog-input_container">
+                            <Image
+                            src='/assets/icons/mail.svg'
+                            alt='mail'
+                            width={18}
+                            height={18}
+                            />
+                            <input
+                            required
+                            type="email"
+                            id='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter your email address"
+                            className="dialog-input"
+                            >
+
+                            </input>
+                          </div>
+
+                          <button 
+                          type="submit"
+                          className="dialog-btn"
+                          >
+                            {
+                              isSubmitting ? 'Submitting...' : 'Track'
+                            }
+                          </button>
+                        </form>
                     </div>
                 </div>
 
